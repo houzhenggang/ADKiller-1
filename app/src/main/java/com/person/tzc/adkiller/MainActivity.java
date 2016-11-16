9caa9d10323437ac12e9c8307189a8cb57fc1028
tongzhichao <tongzhichao@le.com>
2016-11-16 11:23:40 +0800

    Add something

--- app/src/main/java/com/person/tzc/adkiller/MainActivity.java
+++ app/src/main/java/com/person/tzc/adkiller/MainActivity.java
@@ -1,26 +1,37 @@
 package com.person.tzc.adkiller;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.person.tzc.adkiller.service.CleanService;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "MainActivity";
+    private CleanService.CleanBinder mBinder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.start).setOnClickListener(this);
+        findViewById(R.id.bind).setOnClickListener(this);
         findViewById(R.id.stop).setOnClickListener(this);
+        findViewById(R.id.startScan).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.start:
+            case R.id.bind:
+                bind();
+                break;
+            case R.id.startScan:
                 start();
                 break;
             case R.id.stop:
@@ -29,16 +40,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private void start() {
-        Intent intent = new Intent(this, CleanService.class);
-        intent.setFlags(Common.START_SCAN);
-        startService(intent);
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            mBinder = (CleanService.CleanBinder) iBinder;
+        }
 
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
+
+    private void bind() {
+        Logger.d(TAG, "bind");
+        if (mBinder != null) {
+            return;
+        }
+        ComponentName componentName = new ComponentName("com.person.tzc.adkiller", "com.person.tzc.adkiller.service.CleanService");
+        Intent intent = new Intent();
+        intent.setComponent(componentName);
+        startService(intent);
+        bindService(intent, serviceConnection, 0);
+    }
+
+    private void start() {
+        Logger.d(TAG, "start");
+        if (mBinder == null) {
+            Logger.d(TAG, "mBinder == null");
+            return;
+        }
+        if (mBinder.getRootView() == null) {
+            mBinder.setRootView(getWindow().getDecorView());
+        }
+        Logger.d(TAG, "toStartScan");
+        mBinder.toStartScan();
     }
 
     private void stop() {
-        Intent intent = new Intent(this, CleanService.class);
-        intent.setFlags(Common.STOP_SCAN);
-        startService(intent);
+        Logger.d(TAG, "stop");
+        if (mBinder == null) {
+            bind();
+        }
+        mBinder.toStopScan();
     }
 }
--- app/src/main/java/com/person/tzc/adkiller/service/CleanService.java
+++ app/src/main/java/com/person/tzc/adkiller/service/CleanService.java
@@ -1,18 +1,25 @@
 package com.person.tzc.adkiller.service;
 
+import android.app.ActivityManager;
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Binder;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.view.WindowManager;
 
 import com.person.tzc.adkiller.Common;
 import com.person.tzc.adkiller.Logger;
+
+import java.util.List;
 
 /**
  * Created by root on 11/14/16.
@@ -25,8 +32,6 @@ public class CleanService extends Service {
     private Handler mRemoveHandler = new Handler(Looper.getMainLooper());
     private boolean mIsScan = false;
 
-    private View mTView;
-    private ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(0, 0);
     private View mRootView;
 
     private static final int TIME = 1000 * 3;
@@ -34,34 +39,40 @@ public class CleanService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (mTView != null && mTView.getParent() != null) {
-            ((ViewGroup) mTView.getParent()).removeView(mTView);
-        }
-        mTView = new View(getApplicationContext());
+        Logger.d(TAG, "onCreate");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        switch (intent.getFlags()) {
-            case Common.START_SCAN:
-                stratScan();
-                break;
-            case Common.STOP_SCAN:
-                stopScan();
-                break;
-        }
+//        if (intent == null) {
+//            Logger.d(TAG, "START_STICKY");
+//            stratScan();
+//            return START_STICKY;
+//        }
+//        Logger.d(TAG, "onStartCommand: intent.getFlags():" + intent.getFlags() + "  flags:" + flags);
+//        switch (intent.getFlags()) {
+//            case Common.START_SCAN:
+//                stratScan();
+//                break;
+//            case Common.STOP_SCAN:
+//                stopScan();
+//                break;
+//        }
         return START_STICKY;
     }
 
-    private void stratScan() {
+    private void startScan() {
+        Logger.d(TAG, "stratScan");
         if (mIsScan) {
+            Logger.d(TAG, "scanning...");
             return;
         }
         mIsScan = true;
-        mRemoveHandler.postDelayed(checkRunnable,TIME);
+        mRemoveHandler.postDelayed(checkRunnable, TIME);
     }
 
     private void stopScan() {
+        Logger.d(TAG, "stopScan");
         mIsScan = false;
         mRemoveHandler.removeCallbacks(checkRunnable);
     }
@@ -69,52 +80,89 @@ public class CleanService extends Service {
     private Runnable checkRunnable = new Runnable() {
         @Override
         public void run() {
+            Logger.d(TAG, "run");
             if (mIsScan) {
+                Logger.d(TAG, "scan");
                 cleanAd();
-                mRemoveHandler.postDelayed(checkRunnable,TIME);
+                mRemoveHandler.postDelayed(checkRunnable, TIME);
             }
         }
     };
 
     private void cleanAd() {
+        Logger.d(TAG, "cleanAd");
         getAdView();
     }
 
     private View getAdView() {
-        getView();
-        getChildren((ViewGroup) mRootView);
+        Logger.d(TAG, "mRootView:" + mRootView.getClass().getName());
+        getParentView(mRootView);
+//        if (mRootView.getParent() != null) {
+//            Logger.d(TAG, "mRootView:getParent " + mRootView.getRootView().getClass().getName());
+//        } else {
+//            Logger.d(TAG, "mRootView:getParent null");
+//        }
+//        if (mRootView instanceof ViewGroup) {
+//            getChildren((ViewGroup) mRootView);
+//        } else {
+//            Logger.d(TAG, "mRootView:" + mRootView.getClass().getName());
+//        }
+
         return null;
     }
 
+    public void getParentView(View view) {
+        Logger.d(TAG, "p:" + view.getClass().getName());
+        ViewParent p = view.getParent();
+
+        Logger.d(TAG, "pp:" + p.getClass().getName());
+    }
+
     private void getChildren(ViewGroup viewGroup) {
+        Logger.d(TAG, "getChildren");
         int len = viewGroup.getChildCount();
 
         for (int i = 0; i < len; i++) {
             if (viewGroup.getChildAt(i) instanceof ViewGroup) {
                 getChildren((ViewGroup) viewGroup.getChildAt(i));
             } else {
-                String name = viewGroup.getChildAt(i).getClass().getName();
+                String name = viewGroup.getChildAt(i).getClass().getSimpleName();
                 Logger.d(TAG, "view  --  " + name);
             }
         }
 
     }
 
-    private void getView() {
-        if (mRootView != null) {
-            return;
-        }
-        if (mTView == null) {
-            mTView = new View(getApplicationContext());
-        }
-        WindowManager wm = (WindowManager) getApplication().getSystemService(Context.WINDOW_SERVICE);
-        wm.addView(mTView, layoutParams);
-        mRootView = mTView.getRootView();
-    }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return new CleanBinder();
+    }
+
+    public class CleanBinder extends Binder {
+
+        public void setRootView(View view) {
+            mRootView = view;
+        }
+
+        public View getRootView() {
+            return mRootView;
+        }
+
+        public void toStartScan() {
+            startScan();
+        }
+
+        public void toStopScan() {
+            stopScan();
+        }
+
+    }
+
+    private void isHome() {
+        ActivityManager mActivityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> rti = mActivityManager.getRunningTasks(1);
+        ComponentName name = rti.get(0).topActivity;
     }
 }
